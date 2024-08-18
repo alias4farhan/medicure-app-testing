0ef0c67b3dc689cf44c0ff8cd17942c4d32c7da1
alias4farhan <62563476+alias4farhan@users.noreply.github.com>
2024-08-17 21:57:21 -0400

    Update App.java

--- src/main/java/com/testing/insure_me_test_app/App.java
+++ src/main/java/com/testing/insure_me_test_app/App.java
@@ -1,4 +1,4 @@
-package com.testing.insure_me_test_app;
+package comm.testing.medicure_app_testing;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -22,48 +22,43 @@ public class App
 	 	WebDriver driver = new ChromeDriver(options);
 	 	
 	 	//open the web application
-        driver.get("http://18.222.0.17:8081/contact.html");
+        driver.get("http://54.163.195.231:8082/contact.html");
         
         
         String title = driver.getTitle();
         System.out.println(title);
         
         //1. Locate and Enter Name
-        WebElement name = driver.findElement(By.id("inputName"));
-        name.sendKeys("Shubham");
-        
-        String printName = driver.findElement(By.id("inputName")).getText();
-        
-        System.out.println();
+        WebElement name = driver.findElement(By.xpath("/html/body/section/div/div[2]/div[1]/div/div[1]/div[1]/div/input"));
+        name.sendKeys("Farhan");
+	System.out.println(name);
         
         //2. Locate and Enter Contact
-        WebElement number = driver.findElement(By.id("inputNumber"));
-        number.sendKeys("999999999");
-        System.out.println(number.getText().toString());
+        WebElement contact = driver.findElement(By.xpath("/html/body/section/div/div[2]/div[1]/div/div[1]/div[2]/div/input"));
+        contact.sendKeys("123456789");
+	System.out.println(contact);
         
         //3. Locate Email and Enter Email
-        WebElement mail = driver.findElement(By.id("inputMail"));
-        mail.sendKeys("shubham@xyz.com");
-        System.out.println(mail.getText());
+        WebElement mail = driver.findElement(By.xpath("/html/body/section/div/div[2]/div[1]/div/div[2]/input"));
+        mail.sendKeys("abc@abc.com");
+	System.out.println(mail);
         
         //4. Locate Message and Enter Message
-        WebElement message = driver.findElement(By.id("inputMessage"));
-        message.sendKeys("Hello, How are you ?");
-        System.out.println(message.getText());
+        WebElement msg = driver.findElement(By.xpath("/html/body/section/div/div[2]/div[1]/div/div[3]/input"));
+        msg.sendKeys("This is Farhan");
+	System.out.println(msg);
         
         Thread.sleep(2000);
         
         //5. Locate Send Button and click to send the message.
-        WebElement sendButton = driver.findElement(By.id("my-button"));
+        WebElement sendButton = driver.findElement(By.id("message"));
         sendButton.click();
         Thread.sleep(2000);
         
         String response = driver.findElement(By.id("response")).getText();
         System.out.println(response);
-        
-        
+                
         driver.quit();
-
         
     }
 }
